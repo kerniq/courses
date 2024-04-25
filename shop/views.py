c30356c57d7b3f39cc64b9bc1d1b0fc71344eba9
kerniq <kerni0@ya.ru>
2024-04-25 08:47:12 +0300

    add shop application

---
+++ shop/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Course
+
+# Controller
+
+
+def index(request):
+    courses = Course.objects.all()
+    return render(request, 'shop/courses.html', {'courses': courses})
+
+
+def single_course(request, course_id):
+
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'shop/single_course.html', {'course': course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'shop/single_course.html', {'course': course})
